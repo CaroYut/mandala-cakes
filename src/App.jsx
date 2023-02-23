e0310df8b6796f9eeb1c71699adf8a687e41bd12
Carolina Yut <yut.carolina@gmail.com>
2023-02-22 22:23:03 -0300

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {  BrowserRouter, Routes, Route} from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailConteiner";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
-import Error404 from "./components/Error404";
+
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
               <Route path={"/"} element={<ItemListContainer/>} />
               <Route path={"/category/::id"}element={<ItemListContainer/>} />
               <Route path={"/item/::id"} element={<ItemDetailContainer/>} />
-              <Route path={"*"} element={<Error404/>} />
+    
             </Routes>    
         </div>
       </BrowserRouter>
